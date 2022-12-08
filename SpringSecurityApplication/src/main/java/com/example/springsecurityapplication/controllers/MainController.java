6351086351bfa1e672804d8fd195ed9cf21d7010
Кирилл Шишков <shishkov.kirill99@gmail.com>
2022-12-08 23:51:05 +0300

    Вторая проверка работы гита

--- SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/MainController.java
+++ SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/MainController.java
@@ -2,13 +2,31 @@ package com.example.springsecurityapplication.controllers;
 
 import com.example.springsecurityapplication.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/product")
 public class MainController {
 
     private final ProductService productService;
 
     public MainController(ProductService productService) {
         this.productService = productService;
+    }
+
+    // Данный метод предназначен для отображения товаров без прохождения аутентификации и авторизации
+    @GetMapping()
+    public String getAllProduct(Model model) {
+        model.addAttribute("products", productService.getAllProduct());
+        return "product/product";
+    }
+
+    @GetMapping("/product/info/{id}")
+    public String infoProduct(@PathVariable("id") int id, Model model) {
+        model.addAttribute("product", productService.getProductId(id));
+        return "product/infoProduct";
     }
 }
